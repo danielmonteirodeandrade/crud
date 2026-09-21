ca6437ec4de9cad3db9f09c7121eeb318e44c544
rviniciusso <vo83420@gmail.com>
2026-09-21 16:51:45 -0300

    chamada de chamados

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,26 @@
 const API_URL = process.env.API_URL;
 
-function carregarChamados() {
+async function carregarChamados() {
+    const barrafiltro = document.getElementById('filterInput').value.trim();
     const listachamados = document.getElementById("regiao-chamado");
+
+    if (barrafiltro) {
+        API_URL += '?filter=' + encondeURIComponent(barrafiltro);
+    }
     try {
-        
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        listachamados.innerHTML = data.map(chamado => 
+            `<p>${chamado.nome_solicitante}</p>   
+            <p>${chamado.descricao}</p> 
+            <p>${chamado.categoria}</p> 
+            <p>${chamado.prioridade}</p> 
+            <p>${chamado.status}</p> 
+            <p>${chamado.data}</p> 
+            `).join('');
+    } catch (error) {
+        console.error('Erro ao carregar chamados:', error);
     }
 }
 
